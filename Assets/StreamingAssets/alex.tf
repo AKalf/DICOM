83585b913887be7a16a691ee1eaab2458f692a8d
AKalf <foxtrot117@hotmail.com>
2022-06-27 08:06:46 +0300

    alex tfs

---
+++ Assets/StreamingAssets/alex.tf
@@ -0,0 +1,1 @@
+{"version":1,"colourPoints":[{"dataValue":0.5,"colourValue":{"r":0.4690000116825104,"g":0.3540000021457672,"b":0.22300000488758088,"a":1.0}}],"alphaPoints":[{"dataValue":0.009210526011884213,"alphaValue":0.0},{"dataValue":0.5013157725334168,"alphaValue":0.4861111044883728}]}
